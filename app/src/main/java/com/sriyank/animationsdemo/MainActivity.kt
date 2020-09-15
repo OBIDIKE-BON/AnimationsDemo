37f4e36c485d4ef8d12f77932712aedd43810596
OBIDIKE-BON <bonifaceobidike@gmail.com>
2020-09-15 13:54:33 +0100

    property animation commit

--- app/src/main/java/com/sriyank/animationsdemo/MainActivity.kt
+++ app/src/main/java/com/sriyank/animationsdemo/MainActivity.kt
@@ -1,15 +1,13 @@
 package com.sriyank.animationsdemo
 
-import android.animation.Animator
 import android.animation.AnimatorInflater
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity(), Animator.AnimatorListener {
+class MainActivity : AppCompatActivity() {
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
@@ -38,7 +36,7 @@ class MainActivity : AppCompatActivity(), Animator.AnimatorListener {
 	fun translateAnimation(view: View) {
 
 		val translateAnimator = AnimatorInflater.loadAnimator(this, R.animator.translate)
-		translateAnimator?.apply {
+		translateAnimator.apply {
 			setTarget(targetImage)
 			start()
 		}
@@ -47,30 +45,7 @@ class MainActivity : AppCompatActivity(), Animator.AnimatorListener {
 	fun fadeAnimation(view: View) {
 
 		val fadeAnimator = AnimatorInflater.loadAnimator(this, R.animator.alpha)
-		fadeAnimator?.apply {
-			setTarget(targetImage)
-			start()
-		}
-	}
-
-	// Implementation of AnimatorListener interface
-	override fun onAnimationStart(animation: Animator?) {
-
-		Toast.makeText(this, "Animation Started", Toast.LENGTH_SHORT).show()
-	}
-
-	override fun onAnimationRepeat(animation: Animator?) {
-
-		Toast.makeText(this, "Animation Repeated", Toast.LENGTH_SHORT).show()
-	}
-
-	override fun onAnimationEnd(animation: Animator?) {
-
-		Toast.makeText(this, "Animation Ended", Toast.LENGTH_SHORT).show()
-	}
-
-	override fun onAnimationCancel(animation: Animator?) {
-
-		Toast.makeText(this, "Animation Cancelled", Toast.LENGTH_SHORT).show()
+		fadeAnimator.setTarget(targetImage)
+		fadeAnimator.start()
 	}
 }
